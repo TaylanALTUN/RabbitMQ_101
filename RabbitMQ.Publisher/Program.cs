3f7b501c4914113e785743b188f1950ab721320e
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-10 19:31:29 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+//factory.Uri = new Uri("http://localhost:15672/#/");
+factory.Port = 5672;
+factory.HostName = "localhost";
+factory.UserName = "guest";
+factory.Password = "guest";
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    //durable true set edilirse oluşan kuyruklar fiziksel olarak kaydedilir, false olarak set edilirse memoryde tutulur
+    //exclusive true set edilirse burada oluşturulan kanal üserinden bağlanabilir, false set edilirse farklı kanallardan da bağlanılabilir.
+    //auto delete true olarak set edilirse kuyruğa bağlı en son subscriber giderse kuyrukda silinir.
+    channel.QueueDeclare("hello-queue", true, false, false);
+
+    Enumerable.Range(1, 50).ToList().ForEach(x =>
+    {
+        string message = $"Message {x}";
+
+        var messageBody = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(String.Empty, "hello-queue", null, messageBody);
+
+        Thread.Sleep(1500);
+
+        Console.WriteLine($"Mesaj gönderilmiştir: {message}");
+    });
+}
+
+
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+//factory.Uri = new Uri("http://localhost:15672/#/");
+factory.Port = 5672;
+factory.HostName = "localhost";
+factory.UserName = "guest";
+factory.Password = "guest";
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//RabbitMQ den mesajları kaçar kaçar alınacağı belirtiliyor.
+//prefetchSize: Gönderilen mesajın boyutu. 0 set edilirse herhangi boyutta mesaj gönderliebilir.
+//prefetchCount: Tek seferde kaç mesaj geleceği belirtiliyor.
+//global: false set edilirse her bir subscribera prefetchCount da belirtilen değer kadar mesaj gönderilir. true set edilirse prefetchCount da belirlenen adet paylaştırılır.
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+//autoAck true set edilirse rabbitmq subscribera mesaj gönderdiğinde mesaj doğru işlenip işlenmediğine bakmadan kuyruktan siler
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1500);
+
+    Console.WriteLine($"Gelen Mesaj: {message}");
+
+    //Mesajın silinebileceği bildiriliyor.
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+Console.ReadLine();
